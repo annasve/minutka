e10d152eb4d21ff95547b1b8e76a0cb529f7631a
annasve <svejdovaam@gmail.com>
2024-02-01 19:04:26 +0100

    timer interaction, stop countdown button

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('funguju!');
+const alarmElm = document.querySelector('.alarm');
+const audioElm = document.querySelector('audio');
+const resetBtnElm = document.querySelector('.reset-button');
+resetBtnElm.addEventListener('click', stopCountdown);
+
+const userTime =
+  Number(prompt('Nastavte, za kolik vteřin začne minutka zvonit.')) * 1000;
+
+const timerID = setTimeout(ring, userTime);
+
+function ring() {
+  alarmElm.classList.add('alarm--ring');
+  audioElm.play();
+}
+
+function stopCountdown() {
+  clearTimeout(timerID);
+  alert('Zrušeno');
+}
